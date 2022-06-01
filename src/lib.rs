0827f4d787be427126cb440c29330dd1242130dd
James <james@prestwi.ch>
2022-06-01 13:33:50 -0700

    C'est l'heure où les douleurs des malades s'aigrissent!

---
+++ src/lib.rs
@@ -0,0 +1,220 @@
+//! A Gelato relay SDK in rust
+
+#![warn(missing_docs)]
+#![warn(unused_extern_crates)]
+#![forbid(unsafe_code)]
+#![forbid(where_clauses_object_safety)]
+
+mod types;
+use std::str::FromStr;
+
+use reqwest::{IntoUrl, Url};
+pub use types::*;
+
+/// Re-export reqwest for convenience
+pub use reqwest;
+
+use color_eyre::eyre::Result;
+use ethers_core::types::{Bytes, H160, H256, U64};
+use once_cell::sync::Lazy;
+
+const DEFAULT_URL: Lazy<reqwest::Url> =
+    Lazy::new(|| "https://relay.gelato.digital/".parse().unwrap());
+
+/// A Gelato Relay Client
+#[derive(Debug, Clone)]
+pub struct GelatoClient {
+    url: reqwest::Url,
+    client: reqwest::Client,
+}
+
+impl Default for GelatoClient {
+    fn default() -> Self {
+        Self {
+            url: DEFAULT_URL.clone(),
+            client: Default::default(),
+        }
+    }
+}
+
+impl GelatoClient {
+    /// Instantiate a new client with a specific URL
+    ///
+    /// # Errors
+    ///
+    /// If the url param cannot be parsed as a URL
+    pub fn new<S>(url: S) -> Result<Self, reqwest::Error>
+    where
+        S: IntoUrl,
+    {
+        Ok(Self {
+            url: url.into_url()?,
+            ..Default::default()
+        })
+    }
+
+    async fn get(&self, url: Url) -> Result<reqwest::Response, reqwest::Error> {
+        self.client.get(url).send().await
+    }
+
+    /// Instantiate a new client with a specific URL and a reqwest Client
+    ///
+    /// # Errors
+    ///
+    /// If the url param cannot be parsed as a URL
+    pub fn new_with_client<S>(
+        url: S,
+        client: reqwest::Client,
+    ) -> Result<Self, <reqwest::Url as FromStr>::Err>
+    where
+        S: AsRef<str>,
+    {
+        Ok(Self {
+            url: url.as_ref().parse()?,
+            client,
+        })
+    }
+
+    /// Send a transaction over the relay
+    pub async fn send_relay_transaction(
+        &self,
+        chain_id: usize,
+        dest: H160,
+        data: Bytes,
+        fee_token: FeeToken,
+        relayer_fee: U64,
+    ) -> Result<RelayResponse, reqwest::Error> {
+        let params = RelayRequest {
+            dest,
+            data,
+            token: fee_token,
+            relayer_fee,
+        };
+
+        let url = format!("{}/relays/{}", &self.url, chain_id);
+        let res = reqwest::Client::new()
+            .post(url)
+            .json(&params)
+            .send()
+            .await?;
+
+        res.json().await
+    }
+
+    fn send_forward_request_url(&self, chain_id: usize) -> Url {
+        self.url
+            .join("metabox-relays/")
+            .unwrap()
+            .join(&format!("{}", chain_id))
+            .unwrap()
+    }
+
+    /// Send a transaction forward request
+    ///
+    /// <https://docs.gelato.network/developer-products/gelato-relay-sdk/request-types#forwardrequest>
+    ///
+    /// ForwardRequest is designed to handle payments of type 1, 2 and 3, in
+    /// cases where all meta-transaction related logic (or other kinds of
+    /// replay protection mechanisms such as hash based commitments) is already
+    /// implemented inside target smart contract. The sponsor is still required
+    /// to EIP-712 sign this request, in order to ensure the integrity of
+    /// payments. Optionally, nonce may or may not be enforced, by setting
+    /// enforceSponsorNonce. Some dApps may not need to rely on a nonce for
+    /// ForwardRequest if they already implement strong forms of replay
+    /// protection.
+    pub async fn send_forward_request(
+        &self,
+        params: &ForwardRequest,
+    ) -> Result<RelayResponse, reqwest::Error> {
+        self.client
+            .post(self.send_forward_request_url(params.chain_id))
+            .json(&params)
+            .send()
+            .await?
+            .json()
+            .await
+    }
+
+    /// Check if a chain id is supported by Gelato API
+    pub async fn is_chain_supported(&self, chain_id: usize) -> Result<bool, reqwest::Error> {
+        Ok(self.get_gelato_relay_chains().await?.contains(&chain_id))
+    }
+
+    fn relay_chains_url(&self) -> reqwest::Url {
+        self.url.join("relays/").unwrap()
+    }
+
+    /// Get a list of supported chains
+    pub async fn get_gelato_relay_chains(&self) -> Result<Vec<usize>, reqwest::Error> {
+        let res = self.client.get(self.relay_chains_url()).send().await?;
+        Ok(res.json::<RelayChainsResponse>().await?.relays().collect())
+    }
+
+    fn get_estimated_fee_url(
+        &self,
+        chain_id: usize,
+        payment_token: FeeToken,
+        gas_limit: usize,
+        is_high_priority: bool,
+    ) -> Url {
+        let mut url = self
+            .url
+            .join("oracles/")
+            .unwrap()
+            .join(&format!("{}/", chain_id))
+            .unwrap()
+            .join("estimate")
+            .unwrap();
+
+        let payment_token = format!("{:?}", *payment_token);
+
+        url.query_pairs_mut()
+            .append_pair("paymentToken", &payment_token)
+            .append_pair("gasLimit", &gas_limit.to_string())
+            .append_pair("isHighPriority", &is_high_priority.to_string());
+        url
+    }
+
+    /// Get the estimated fee for a specific amount of gas on a specific chain,
+    /// denominated in a specific payment token./
+    ///
+    ///
+    pub async fn get_estimated_fee(
+        &self,
+        chain_id: usize,
+        payment_token: impl Into<FeeToken>,
+        gas_limit: usize,
+        is_high_priority: bool,
+    ) -> Result<usize, reqwest::Error> {
+        let url =
+            self.get_estimated_fee_url(chain_id, payment_token.into(), gas_limit, is_high_priority);
+
+        Ok(reqwest::get(url)
+            .await?
+            .json::<EstimatedFeeResponse>()
+            .await?
+            .estimated_fee())
+    }
+
+    fn get_task_status_url(&self, task_id: H256) -> Url {
+        self.url
+            .join("/tasks/GelatoMetaBox/")
+            .unwrap()
+            .join(&format!("{:?}/", task_id))
+            .unwrap()
+    }
+
+    /// Fetch the status of a task
+    pub async fn get_task_status(
+        &self,
+        task_id: H256,
+    ) -> Result<Option<TransactionStatus>, reqwest::Error> {
+        Ok(self
+            .get(self.get_task_status_url(task_id))
+            .await?
+            .json::<TaskStatusResponse>()
+            .await?
+            .into_iter()
+            .next())
+    }
+}
---
+++ src/types.rs
@@ -0,0 +1,304 @@
+use ethers_core::types::{Bytes, H160, H256, U256, U64};
+use once_cell::unsync::Lazy;
+use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+/// Magic value used to specify the chain-native token
+pub const NATIVE_TOKEN: Lazy<FeeToken> = Lazy::new(|| {
+    FeeToken(
+        "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+            .parse()
+            .unwrap(),
+    )
+});
+
+/// A gelato fee token is an ERC20 address, which defaults to `0xee..ee`. This
+/// magic value indicates "eth" or the native asset of the chain. This FeeToken
+/// must be allowlisted by Gelato validators
+#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq)]
+pub struct FeeToken(H160);
+
+impl std::ops::Deref for FeeToken {
+    type Target = H160;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Default for FeeToken {
+    fn default() -> Self {
+        *NATIVE_TOKEN
+    }
+}
+
+impl From<H160> for FeeToken {
+    fn from(token: H160) -> Self {
+        Self(token)
+    }
+}
+
+/// Gelato payment type
+/// <https://docs.gelato.network/developer-products/gelato-relay-sdk/payment-types>
+#[derive(Debug, Copy, Clone, Serialize_repr, Deserialize_repr, PartialEq, Eq)]
+#[repr(u8)]
+pub enum PaymentType {
+    /// The target smart contract will pay Gelato Relay's smart contract as the
+    /// call is forwarded. Payment can be done in feeToken, where it is
+    /// expected to be a whitelisted payment token.
+    Synchronous = 0,
+    /// The sponsor must hold a balance in one of Gelato's Gas Tank smart
+    /// contracts. The balance could even be held on a different chainId than
+    /// the one the transaction is being relayed on (as defined by
+    /// sponsorChainId).
+    ///
+    /// An event is emitted to tell Gelato how much to charge in the future,
+    /// which shall be acknowledged in an off-chain accounting system. A
+    /// sponsor signature is expected in order to ensure that the sponsor
+    /// agrees on being charged up to a maxFee amount
+    AsyncGasTank = 1,
+    /// Similar to Type 1, but sponsor is expected to hold a balance with
+    /// Gelato on the same chainId where the transaction is executed. Fee
+    /// deduction happens during the transaction. A sponsor signature is
+    /// expected in order to ensure that the sponsor agrees on being charged up
+    /// to a maxFee amount.
+    SyncGasTank = 2,
+    /// In this scenario a sponsor pre-approves the appropriate Gelato Relay's
+    /// smart contract to spend tokens up so some maximum allowance value.
+    /// During execution of the transaction, Gelato will credit due fees using
+    /// `IERC20(feeToken).transferFrom(...)` in order to pull fees from his/her
+    /// account. A sponsor signature is expected in order to ensure that the
+    /// sponsor agrees on being charged up to a maxFee amount.
+    SyncPullFee = 3,
+}
+
+/// Request for forwarding tx to gas-tank based relay service.
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct ForwardRequest {
+    /// must be exactly "ForwardRequest"
+    pub type_id: &'static str,
+    /// Chain id
+    pub chain_id: usize,
+    /// Address of dApp's smart contract to call.
+    pub target: H160,
+    /// Payload for `target`.
+    pub data: Bytes,
+    /// paymentToken for Gelato Executors
+    pub fee_token: H160,
+    ///Type identifier for Gelato's payment. Can be 1, 2 or 3.
+    pub payment_type: usize, // 1 = gas tank
+    /// Maximum fee sponsor is willing to pay Gelato Executors
+    pub max_fee: U64,
+    /// Gas limit
+    pub gas: U64,
+    /// EOA address that pays Gelato Executors.
+    pub sponsor: H160,
+    /// Chain ID of where sponsor holds a Gas Tank balance with Gelato
+    /// Usually the same as `
+    pub sponsor_chain_id: usize,
+    /// Smart contract nonce for sponsor to sign.
+    /// Can be 0 if enforceSponsorNonce is always false.
+    pub nonce: usize,
+    /// Whether or not to enforce replay protection using sponsor's nonce.
+    /// Defaults to false, as repla
+    pub enforce_sponsor_nonce: bool,
+    /// Whether or not ordering matters for concurrently submitted transactions.
+    /// Defaults to `true` if not provided.
+    pub enforce_sponsor_nonce_ordering: Option<bool>,
+    /// EIP-712 signature over the forward request
+    pub sponsor_signature: ethers_core::types::Signature,
+}
+
+/// A Relay Request
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct RelayRequest {
+    /// The address of the contract to be called
+    pub dest: H160,
+    /// The calldata
+    pub data: Bytes,
+    /// The fee token
+    pub token: FeeToken,
+    /// The amount of fee
+    pub relayer_fee: U64,
+}
+
+/// An Estimated Fee Request
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct EstimatedFeeRequest {
+    /// Payment token
+    pub payment_token: FeeToken,
+    /// Gas limit
+    pub gas_limit: U64,
+    /// Whether this is high priority
+    pub is_high_priority: bool,
+}
+
+/// Response to relay request, contains an ID for the task
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct RelayResponse {
+    /// The task ID
+    pub task_id: H256,
+}
+
+/// Response to estimated fee request. Contains the estimated fee
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct EstimatedFeeResponse {
+    /// The oracle-recommended fee, as a decimal string
+    estimated_fee: String,
+}
+
+impl EstimatedFeeResponse {
+    pub(crate) fn estimated_fee(&self) -> usize {
+        self.estimated_fee.parse().unwrap()
+    }
+}
+
+/// Response to Relay chains request. Contains a list of chain ids supported
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct RelayChainsResponse {
+    /// The supported chain ids
+    relays: Vec<String>,
+}
+
+impl RelayChainsResponse {
+    pub(crate) fn relays<'a>(&'a self) -> impl Iterator<Item = usize> + 'a {
+        self.relays.iter().map(|s| s.parse().unwrap())
+    }
+}
+
+/// Response to the GetTaskStatus api call. Contains an array of task statuses
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct TaskStatusResponse {
+    data: Vec<TransactionStatus>,
+}
+
+impl std::ops::Deref for TaskStatusResponse {
+    type Target = Vec<TransactionStatus>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl IntoIterator for TaskStatusResponse {
+    type Item = TransactionStatus;
+
+    type IntoIter = <Vec<TransactionStatus> as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.data.into_iter()
+    }
+}
+
+/// A Task Status object
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct TransactionStatus {
+    /// Service name
+    pub service: String,
+    /// Chain name
+    pub chain: String,
+    /// Task id
+    pub task_id: H256,
+    /// Task state
+    pub task_state: TaskState,
+    /// Created at date/time string
+    #[serde(rename = "created_at")]
+    pub created_at: String, // date
+    /// Info from last check
+    pub last_check: Option<CheckOrDate>,
+    /// Execution info
+    pub execution: Option<Execution>,
+    /// Last execution date/time string
+    pub last_execution: String, // date
+}
+
+/// Execution details
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Execution {
+    /// Transaction status
+    pub status: String,
+    /// Transaction hash
+    pub transaction_hash: H256,
+    /// Block number
+    pub block_number: usize,
+    /// Creation date/time string
+    #[serde(rename = "created_at")]
+    pub created_at: String,
+}
+
+/// Either check details, or a date/time string
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(untagged, rename_all = "camelCase")]
+pub enum CheckOrDate {
+    /// Date
+    Date(String),
+    /// Check
+    Check(Check),
+}
+
+/// Check info for a
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Check {
+    /// Task state at this check
+    pub task_state: TaskState,
+    /// Message string
+    pub message: Option<String>,
+    /// Creation date/time string
+    #[serde(rename = "created_at")]
+    pub created_at: Option<String>,
+}
+
+/// Transaction payload information
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct Payload {
+    /// Transaction target
+    pub to: H160,
+    /// Transaction input data
+    pub data: Bytes,
+    /// Fee data
+    pub fee_data: FeeData,
+}
+
+/// eip1559 fee data
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct FeeData {
+    /// Gas Price
+    pub gas_price: U256,
+    /// Max fee per gas
+    pub max_fee_per_gas: U256,
+    /// Max priority fee per gas
+    pub max_priority_fee_per_gas: U256,
+}
+
+/// Task states
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum TaskState {
+    /// CheckPending
+    CheckPending,
+    /// ExecPending
+    ExecPending,
+    /// ExecSuccess
+    ExecSuccess,
+    /// ExecReverted
+    ExecReverted,
+    /// WaitingForConfirmation
+    WaitingForConfirmation,
+    /// Blacklisted
+    Blacklisted,
+    /// Cancelled
+    Cancelled,
+    /// NotFound
+    NotFound,
+}
